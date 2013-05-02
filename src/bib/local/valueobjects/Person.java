f30b3eca2fd51ba977f9f70a71170b86c32428b2
Florian <pulv3rtoastmann@gmail.com>
2013-05-02 21:12:52 +0200

    Personenklasse mit überrschreibender equals Methode

--- src/bib/local/valueobjects/Person.java
+++ src/bib/local/valueobjects/Person.java
@@ -7,7 +7,9 @@ public class Person {
 	private String strasse = "";
 	private String plz = "";
 	private String wohnort = "";
-
+	
+	
+	//Konstruktor
 	public Person(int nr, String name, String anrede, String strasse, String plz, String ort){
 		this.nummer = nr;
 		this.name = name;
@@ -15,11 +17,49 @@ public class Person {
 		this.strasse = strasse;
 		this.plz = plz;
 		this.wohnort = ort;
-	
 	}
 	
+	@Override
+	/**
+	 * Standard-Methode von Object überschrieben.
+	 * Methode dient Vergleich von zwei Personen-Objekten anhand ihrer Werte,
+	 * d.h. nummer und name.
+	 * 
+	 * @see java.lang.Object#equals()
+	 */
+	public boolean equals(Object anderePerson){
+		if (anderePerson instanceof Person)
+			return (( nummer == ((Person) anderePerson).nummer)
+					&& name.equals(((Person) anderePerson).name));
+		else
+			return false;
+		
+	}
+	
+	
+	//Accessor-Methoden
 	public int getNummer(){
 		return nummer;
+	}
+	
+	public String getAnrede(){
+		return anrede;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public String getStraße(){
+		return strasse;
+	}
+	
+	public String getPlz(){
+		return plz;
+	}
+	
+	public String getWohnort(){
+		return wohnort;
 	}
 
 }
