e2b06994e37e838f254fd9d5795a75f3645fa97b
Florian <pulv3rtoastmann@gmail.com>
2013-05-02 16:39:52 +0200

    personenklasse

--- src/bib/local/valueobjects/Person.java
+++ src/bib/local/valueobjects/Person.java
@@ -1,5 +1,21 @@
 package bib.local.valueobjects;
 
 public class Person {
+	private int nummer;
+	private String anrede="";
+	private String name="";
+	private String strasse = "";
+	private String plz = "";
+	private String wohnort = "";
+
+	public Person(int nr, String name, String anrede, String strasse, String plz, String ort){
+		this.nummer = nr;
+		this.name = name;
+		this.anrede = anrede;
+		this.strasse = strasse;
+		this.plz = plz;
+		this.wohnort = ort;
+		
+	}
 
 }
