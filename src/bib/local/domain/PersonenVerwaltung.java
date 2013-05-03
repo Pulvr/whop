3c48cf7953f7c87120876608bd1d122a9b0b19cb
Pulvr <pulv3rtoastmann@gmail.com>
2013-05-04 00:04:30 +0300

    Update PersonenVerwaltung.java

--- src/bib/local/domain/PersonenVerwaltung.java
+++ src/bib/local/domain/PersonenVerwaltung.java
@@ -1,1 +1,22 @@
-s
+package bib.local.domain;
+
+import java.util.Vector;
+
+import bib.local.persistence.FilePersistenceManager;
+import bib.local.persistence.PersistenceManager;
+import bib.local.valueobjects.Person;
+
+//
+
+/**
+ * Klasse zur Verwaltung von Personen (Kunde und Mitarbeiter)
+ * @author Florian
+ *
+ */
+
+public class PersonenVerwaltung {
+  
+	private Vector<Person> personenListe = new Vector<Person>();
+	
+	private PersistenceManager pm = new FilePersistenceManager();
+}
