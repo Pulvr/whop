322ab0abecbfb696aa8064c5c96d54cdea907932
mimiviech <mofischer@stud.hs-bremen.de>
2013-09-22 10:40:23 +0200

    erneuter Massengutpush

---
+++ src/bib/local/valueobjects/MassengutWare.java
@@ -0,0 +1,42 @@
+package bib.local.valueobjects;
+
+
+public class MassengutWare extends Ware {
+    private static final long serialVersionUID = 1L;
+    
+    private int packungsGroesse = 0;
+
+    public MassengutWare(String bezeichnung, int nr, int bstd, float preis, int packungsGroesse) {
+        super(bezeichnung, nr, bstd, preis);
+        this.packungsGroesse = packungsGroesse;
+    }
+
+    public int getPackungsGroesse() {
+        return packungsGroesse;
+    }
+
+    public void setPackungsGroesse(int packungsGroesse) {
+        this.packungsGroesse = packungsGroesse;
+    }
+    
+    /**
+     * Standard-Methode von Object überschrieben.
+     * Methode wird immer automatisch aufgerufen, wenn ein Waren-Objekt als String
+     * benutzt wird (z.B. in println(Ware);)
+     * 
+     * @see java.lang.Object#toString()
+     */
+    public String toString() {
+        String ausgabe = super.toString(false);
+        return ausgabe + " / Packungsgröße: " + packungsGroesse + "\n";
+    }
+
+    @Override
+    public boolean checkBestellmengeGueltig(int menge) {
+        return (menge % packungsGroesse == 0);
+    }
+    
+    public float getRechnungsPreis(){
+        return getPreis() * getPackungsGroesse();
+    }
+}
